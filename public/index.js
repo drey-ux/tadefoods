a7ee6594cecd727268e714e8ce749cde0636a9ba
Damilare <damilareoyetade1@gmail.com>
2020-10-24 18:47:39 +0100

    first commit

---
+++ public/index.js
@@ -0,0 +1,10 @@
+var burger = document.getElementById('burger');
+var menu = document.getElementById('menu');
+
+burger.addEventListener('click', () => {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+});
